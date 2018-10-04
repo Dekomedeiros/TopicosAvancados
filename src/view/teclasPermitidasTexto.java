fe4b6e10f5ba8f690f1bb8521daeedaa8657fa41
Andre Medeiros <amedeiros.leandro@gmail.com>
2018-10-04 18:35:06 -0300

    Adicionados acentos e espaço

--- src/view/teclasPermitidasTexto.java
+++ src/view/teclasPermitidasTexto.java
@@ -6,9 +6,9 @@ import javax.swing.text.PlainDocument;
 public class teclasPermitidasTexto extends PlainDocument{
 	@Override
 	public void insertString(int offset, String str, javax.swing.text.AttributeSet attr) throws BadLocationException{
-		super.insertString(offset, str.replaceAll("[^a-z|^A-Z|^ç|^~|^ ]", ""), attr);
+		super.insertString(offset, str.replaceAll("[^a-z|^A-Z|^ç|^~|^ |^´]", ""), attr);
 	}
 	public void replace(int offset, String str, javax.swing.text.AttributeSet attr) throws BadLocationException{
-		super.insertString(offset, str.replaceAll("[^a-z|^A-Z|^ç|^~|^ ]", ""), attr);
+		super.insertString(offset, str.replaceAll("[^a-z|^A-Z|^ç|^~|^ |^´]", ""), attr);
 	}
 }
